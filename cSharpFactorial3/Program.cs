70abb1ed4f1c32d8000bf4acb5cf5d712146742d
nimna nilwala <nimna.nilwala555@gmail.com>
2023-11-22 23:17:58 +0530

    Add project files.

---
+++ cSharpFactorial3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace cSharpFactorial3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i, fact=1;
+            Console.Write("Enter a number...: ");
+            num = int.Parse(Console.ReadLine());
+
+            for(i=num; i>0; i--)
+            {
+                fact = fact *i;
+            }
+            Console.WriteLine(fact);
+
+        }
+    }
+}
